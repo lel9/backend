d3629d5f37671ff8126cfd40192723a5113ebbb6
olga <olg-199774@mail.ru>
2019-03-20 23:16:38 +0300

    EJ-23: add user repository

---
+++ src/main/java/testsystem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package testsystem.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import testsystem.domain.User;
+
+import java.util.UUID;
+
+public interface UserRepository extends CrudRepository<User, UUID> {
+    User findByUsername(String username);
+}
