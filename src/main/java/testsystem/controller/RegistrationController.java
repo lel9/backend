c95b86cd9d452d87fc9311830b556d9c722b5dd9
olga <olg-199774@mail.ru>
2019-03-21 14:09:54 +0300

    EJ-23: add registration controller

---
+++ src/main/java/testsystem/controller/RegistrationController.java
@@ -0,0 +1,28 @@
+package testsystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import testsystem.domain.User;
+import testsystem.dto.UserDTO;
+import testsystem.service.UserServiceImpl;
+
+import javax.validation.Valid;
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @PostMapping("/sign-up")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO signUp(@RequestBody @Valid UserDTO user) {
+        User registeredUser = userService.registerUser(User.fromUserDTO(user));
+        return new UserDTO(registeredUser.getUsername(), registeredUser.getEmail());
+    }
+
+}
