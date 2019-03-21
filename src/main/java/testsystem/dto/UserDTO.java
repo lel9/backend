84d5a9ff0c6f41833f8c5957a2fa6e117a0a8f37
olga <olg-199774@mail.ru>
2019-03-21 12:29:11 +0300

    EJ-23: add password match check

--- src/main/java/testsystem/dto/UserDTO.java
+++ src/main/java/testsystem/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package testsystem.dto;
 
 import lombok.*;
+import testsystem.validator.PasswordMatches;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
@@ -9,6 +10,7 @@ import javax.validation.constraints.NotNull;
 
 @Data
 @NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+@PasswordMatches
 public class UserDTO {
 
     public UserDTO(String username, String email) {
---
+++ src/main/java/testsystem/validator/PasswordMatchesValidator.java
@@ -0,0 +1,21 @@
+package testsystem.validator;
+
+import testsystem.dto.UserDTO;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public void initialize(final PasswordMatches constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(final Object obj, final ConstraintValidatorContext context) {
+        final UserDTO user = (UserDTO) obj;
+        return user.getPassword() != null && user.getPassword().equals(user.getConfirmPassword());
+    }
+
+}
