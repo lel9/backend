a696780f721c5b9f5461bd53bc827eecf9e9cb7a
olga <olg-199774@mail.ru>
2019-03-21 16:20:31 +0300

    EJ-23: add view for json filtration

--- src/main/java/testsystem/dto/UserDTO.java
+++ src/main/java/testsystem/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package testsystem.dto;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,17 +22,21 @@ public class UserDTO {
 
     @NotNull
     @NotEmpty
+    @JsonView({View.REST.class, View.UI_AUTH.class, View.UI_REG.class})
     private String username;
 
     @NotNull
     @NotEmpty
     @Email
+    @JsonView(View.UI_REG.class)
     private String email;
 
     @NotNull
     @NotEmpty
+    @JsonView({View.REST.class})
     private String password;
 
+    @JsonView({View.UI_AUTH.class, View.UI_REG.class})
     private String role;
 
 }
---
+++ src/main/java/testsystem/dto/View.java
@@ -0,0 +1,7 @@
+package testsystem.dto;
+
+public class View {
+    public static class REST {}
+    public static class UI_AUTH {}
+    public static class UI_REG {}
+}
