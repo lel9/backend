8c200877cf75ac7da922688dc217eef6f095a5db
olga <olg-199774@mail.ru>
2019-03-23 00:28:16 +0300

    EJ-24: done

--- src/main/java/testsystem/dto/View.java
+++ src/main/java/testsystem/dto/View.java
@@ -2,6 +2,7 @@ package testsystem.dto;
 
 public class View {
     public static class REST {}
-    public static class UI_AUTH {}
-    public static class UI_REG {}
+
+    public static class UI {
+    }
 }
