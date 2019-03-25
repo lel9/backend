44d267fa50ef52a617c92bd019c2fa2e227bab4e
olga <olg-199774@mail.ru>
2019-03-26 00:48:57 +0300

    EJ-45: add category domain

---
+++ src/main/java/testsystem/domain/Category.java
@@ -0,0 +1,27 @@
+package testsystem.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class Category {
+    @Id
+    @GeneratedValue
+    private final UUID id = UUID.randomUUID();
+
+    private String name;
+}
