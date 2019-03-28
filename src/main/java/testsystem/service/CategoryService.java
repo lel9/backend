3bc963ae3499a43167bdf9a96a98e4679715c119
olga <olg-199774@mail.ru>
2019-03-28 14:35:19 +0300

    tasks-list-without-pagination: done

--- src/main/java/testsystem/service/CategoryService.java
+++ src/main/java/testsystem/service/CategoryService.java
@@ -2,9 +2,12 @@ package testsystem.service;
 
 import testsystem.dto.CategoryDTO;
 import testsystem.dto.CategoryListDTO;
+import testsystem.dto.TaskListDTO;
 
 public interface CategoryService {
     void addNewCategory(CategoryDTO categoryDTO);
 
     CategoryListDTO getCategoriesList();
+
+    TaskListDTO getTasksList(String id);
 }
